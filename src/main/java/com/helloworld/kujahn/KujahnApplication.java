672ede5499c8f727ae5957737a0212ad66ceb476
kujaHn <rnwkgus2@gmail.com>
2021-02-16 21:06:56 +0900

    Initial commit

---
+++ src/main/java/com/helloworld/kujahn/KujahnApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.kujahn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KujahnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KujahnApplication.class, args);
+    }
+
+}
